151892dbd5f58afa49a7456f547f900f06f338c1
B1gD0g1 <154958364+B1gD0g1@users.noreply.github.com>
2025-11-26 16:17:22 +0800

    添加项目文件。

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly AppDbContext context;
+
+        public ActivitiesController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+        {
+            var activities = await context.Activities.FindAsync(id);
+
+            if (activities == null) return NotFound();
+
+            return Ok(activities);
+        }
+    }
+}
